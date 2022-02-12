4b531676cc16afbeb42040397f9441c9333bdda9
Ali Dhanani <ali.dhanani@nextauth.com>
2022-02-12 15:40:06 +0100

    Creating Alert for Victory

---
+++ TicTacToeGame/TicTacToeGame/TicTacAlert.swift
@@ -0,0 +1,28 @@
+//
+//  TicTacAlert.swift
+//  TicTacToeGame
+//
+//  Created by NextAuth on 12/02/2022.
+//
+
+import Foundation
+import UIKit
+
+class TicTacAlert {
+    // Making Singelton For Alert
+    static let shared = TicTacAlert()
+    
+    init() {
+    }
+    
+    func finalResultAlert(Title title: String, Cross cross: String, Zero zero: String, CurrentView show: UIViewController ,closure: @escaping (()->Void))
+    {
+        let message = "\nZeros " + String(zero) + "\n\nCrosses " + String(cross)
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            closure()
+        }))
+        show.present(ac, animated: true)
+    }
+    
+}
