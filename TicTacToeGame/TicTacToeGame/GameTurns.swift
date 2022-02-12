c23ea8132890af1a8583c4a9fc66d7f9bbee4218
Ali Dhanani <ali.dhanani@nextauth.com>
2022-02-12 15:36:18 +0100

    Creating Players

---
+++ TicTacToeGame/TicTacToeGame/GameTurns.swift
@@ -0,0 +1,14 @@
+//
+//  GameTurns.swift
+//  tictactoe
+//
+//  Created by NextAuth on 12/02/2022.
+//
+
+import Foundation
+
+// Creating 2 players X and O
+enum GameTurns {
+    case Zero
+    case Cross
+}
